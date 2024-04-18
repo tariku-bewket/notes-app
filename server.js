5c00c947dd19ee00c422ce51003d98418b585750
Tariku Bewket <tarikubew@gmail.com>
2024-04-18 15:32:07 +0100

    error handler and logger

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const path = require('path');
+const cors = require('cors');
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+
+app.use(logger);
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 
@@ -20,6 +32,7 @@ app.use('*', (req, res) => {
   }
 });
 
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
